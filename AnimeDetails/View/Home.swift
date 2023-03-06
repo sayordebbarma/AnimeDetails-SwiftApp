5fbf15fcd5fff477bcec3f536e30c63e57d8457c
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-03-06 22:15:17 +0530

    added tagView

--- AnimeDetails/View/Home.swift
+++ AnimeDetails/View/Home.swift
@@ -8,10 +8,66 @@
 import SwiftUI
 
 struct Home: View {
+    @State private var activeGerne: String = "Romance"
+    @Namespace private var animation
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 15) {
+            HStack {
+                Text("Anime")
+                    .font(.largeTitle.bold())
+                Text("Recommended")
+                    .fontWeight(.semibold)
+                    .padding(.leading, 15)
+                    .foregroundColor(.gray)
+                    .offset(y: 2)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 15)
+            
+            gerneView()
+            
+            
+        }
+    }
+        
+        @ViewBuilder
+        func gerneView() -> some View {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 10) {
+                    ForEach(gernes, id: \.self) { gerne in
+                        Text(gerne)
+                            .font(.caption)
+                            .padding(.horizontal, 12)
+                            .padding(.vertical, 5)
+                            .background {
+                                if activeGerne == gerne {
+                                    Capsule()
+                                        .fill(Color("Blue"))
+                                        .matchedGeometryEffect(id: "ACTIVETAB", in: animation)
+                                } else {
+                                    Capsule()
+                                        .fill(.gray.opacity(0.2))
+                                }
+                            }
+                            .foregroundColor(activeGerne == gerne ? .white : .gray)
+                        //change activeGerne on tap
+                            .onTapGesture {
+                                withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.7)) {
+                                    activeGerne = gerne
+                                }
+                            }
+                }
+            }
+                .padding(.horizontal, 15)
+        }
     }
 }
+
+//gernes
+var gernes: [String] = [
+    "Action", "Ecchi", "Romance", "School", "Adventure", "Drama", "Comedy", "Demons"
+]
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
