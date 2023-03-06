c317e14f5d45971ad05af11c8ed8c53bf374fb67
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-03-06 21:18:43 +0530

    Initial Commit

---
+++ AnimeDetails/AnimeDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeDetailsApp.swift
+//  AnimeDetails
+//
+//  Created by Sayor Debbarma on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimeDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
